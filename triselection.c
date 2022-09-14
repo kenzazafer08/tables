1104f3adbb1fb9776d6f7fd10c86f0a5872f8026
unknown <zaaferkenza08@gmail.com>
2022-09-14 15:30:18 +0100

    done

--- triselection.c
+++ triselection.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #define D 10
 int main(){
-    int j,i,N,c=0;
+    int i,N,max,pos;
     int tab[D];
+    int m;
      printf("Combien de nombre vous voulez stocker (max : 10) : ");
     scanf("%d",&N);
+    m=N;
     for(i=0;i<N;i++){
         printf("case [%d] : ",i+1);
         scanf("%d",&tab[i]);
@@ -14,16 +16,24 @@ int main(){
         printf("\n%d : %d",i+1,tab[i]);
        }
     printf("\n----Votre tableau triee----");
-    for(i=0;i<N-1;i++){
-        for(j=i+1;j<N;j++){
-            if ( tab[i] > tab[j] ) {
-            c = tab[i];
-            tab[i] = tab[j];
-            tab[j] = c;
-          } 
+    while (N>0)
+    { 
+        max=tab[0];
+        pos=0;
+        for(i=0;i<N;i++){
+            if(tab[i]>max){
+             max=tab[i];
+             pos=i;
+            }
         }
+        for(i=pos;i<N;i++){
+            tab[i]=tab[i+1];
+        }
+        tab[N-1]=max;
+        N--;
     }
-    for(i=0;i<N;i++){
+
+    for(i=0;i<m;i++){
         printf("\n%d : %d",i+1,tab[i]);
        }
     }
