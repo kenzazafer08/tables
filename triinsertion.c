4c2abf73c87ab20b36c906b00c1bd2a2bdb4b741
unknown <zaaferkenza08@gmail.com>
2022-09-14 15:43:19 +0100

    done

--- triinsertion.c
+++ triinsertion.c
@@ -14,14 +14,15 @@ int main(){
         printf("\n%d : %d",i+1,tab[i]);
        }
     printf("\n----Votre tableau triee----");
-    for(i=1;i<=N-1;i++){
-        j=i;
-        while (j > 0 && tab[j-1] > tab[j]) {
-         c = tab[j];
-         tab[j] = tab[j-1];
-         tab[j-1] = c;
+    for(i=0;i<N;i++){
+        c=tab[i];
+        j=i-1;
+        while (j >= 0 && tab[j] > c) {
+        
+         tab[j+1] = tab[j];
+        
       j--;
-    }
+    }tab[j+1]=c;
     }
     for(i=0;i<N;i++){
         printf("\n%d : %d",i+1,tab[i]);
